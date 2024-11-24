7c33c4656bdcc07a3ddca834fa8f8424549af376
mydevify <boussaadilouay9@gmail.com>
2024-11-24 22:07:37 +0100

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className="py-4 mt-4 mb-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-sm">
-          &copy; {new Date().getFullYear()} GestionLocatoins. All rights reserved.
+          &copy; {new Date().getFullYear()} Reantia. All rights reserved.
         </div>
         <div className="text-sm">
-          Made with ❤️ by <Link href={'https://mydevify.com'}>xx</Link> 
+          <Link href={'https://mydevify.com'}> Made with ❤️</Link> 
         </div>
       </div>
     </footer>
