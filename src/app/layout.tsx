97d093217fbdfd457dd6af0f4de537394552a622
mydevify <boussaadilouay9@gmail.com>
2024-11-25 01:00:09 +0100

    wrapped page with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const geistSans = localFont({
@@ -28,10 +29,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={inter.className}>
         {children} 
       </body>
     </html>
+    </ClerkProvider>
   );
 }
